8200310f6b60caf8f77586ebafe642a4434f52f1
PRINCE KUAMR <gmprince420@gmail.com>
2025-11-14 23:45:29 +0530

    Working With frontend

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [comment,  setComment] = useState("");
 
+  useEffect(()=> {
+    axios.post("http://localhost:3000/home", {}, {withCredentials: true}).then((res)=> setComment(res.data))
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        name="email"
+        placeholder="Enter your email "
+      />
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        name="password"
+        placeholder="Enter your password"
+      />
+      <button
+        type="submit"
+        onClick={() => {
+          axios
+            .post(
+              "http://localhost:3000/signin",
+              { email, password },
+              {
+                withCredentials: true,
+              }
+            )
+            .then((res) => {
+              alert(res.data);
+            });
+        }}
+      >
+        submit
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          axios
+            .post(
+              "http://localhost:3000/logout",
+              { email, password },
+              {
+                withCredentials: true,
+              }
+            )
+            .then((res) => {
+              alert(res.data);
+            });
+        }}
+      >
+        logout
+      </button>
+      <span>{comment}</span>
+    </div>
+  );
 }
 
-export default App
+export default App;
